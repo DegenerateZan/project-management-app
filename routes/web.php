a98f8e248f26ee9559a50a508a2b0a22a50961e4
ekasaputra0324 <paijomr4221@gmail.com>
2022-04-10 18:54:01 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard.dashboard', );
 });
 Route::get('/projects', function () {
